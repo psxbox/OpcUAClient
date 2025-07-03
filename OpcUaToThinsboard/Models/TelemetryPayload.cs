2fc04b46c2518cd223c931f4d7510e737545e8b7
Adham Yusupov <adhamyu83@gmail.com>
2025-07-03 16:22:31 +0500

    Добавить атрибут JsonPropertyName для свойства Values в классе TelemetryPayload

--- OpcUaToThinsboard/Models/TelemetryPayload.cs
+++ OpcUaToThinsboard/Models/TelemetryPayload.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace OpcUaToThinsboard.Models;
 
 public class TelemetryPayload
 {
+    [JsonPropertyName("ts")]
     public long Ts { get; set; }
-    public Dictionary<string, object> Values { get; set; } = [];
+
+    [JsonPropertyName("values")]
+    public Dictionary<string, object> Values { get; set; } = new();
 }
